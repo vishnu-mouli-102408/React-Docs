e4543fa50268fc5d2cf7339ab332ea1c84b116d9
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-29 07:28:57 +0000

    JSX expressions practise

--- src/index.js
+++ src/index.js
@@ -4,13 +4,24 @@ import ReactDom from "react-dom";
 const fName = "Mouli";
 const lName = "Ganivada";
 
+const d = new Date();
+let year = d.getFullYear();
+
+// ReactDom.render(
+//   <div>
+//     <h1>Hello {fName + " " + lName}!</h1>
+//     <h1>
+//       Hello {fName} {lName}!
+//     </h1>
+//     <h1> Hello {`${fName} ${lName}`}!</h1>
+//   </div>,
+//   document.getElementById("root")
+// );
+
 ReactDom.render(
   <div>
-    <h1>Hello {fName + " " + lName}!</h1>
-    <h1>
-      Hello {fName} {lName}!
-    </h1>
-    <h1> Hello {`${fName} ${lName}`}!</h1>
+    <p> Created by {`${fName} ${lName}!`} </p>
+    <p> Copyright {year}</p>
   </div>,
   document.getElementById("root")
 );
