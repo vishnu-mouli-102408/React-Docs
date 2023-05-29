435d408570af2389d6eb605377b1abd75d0167ac
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-29 07:20:27 +0000

    JSX Expressions

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-
-// import App from "./App";
-
-// const rootElement = document.getElementById("root");
-// const root = createRoot(rootElement);
-
-// root.render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>
-// );
-
 import React from "react";
 import ReactDom from "react-dom";
 
-// const React = require("react");
-// const ReactDom = require("react-dom");
+const fName = "Mouli";
+const lName = "Ganivada";
 
 ReactDom.render(
   <div>
-    <h1>My Favourite Food Items</h1>
-    <ul>
-      <li>Pasta</li>
-      <li>Biryani</li>
-      <li>Chicken</li>
-    </ul>
+    <h1>Hello {fName + " " + lName}!</h1>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <h1> Hello {`${fName} ${lName}`}!</h1>
   </div>,
   document.getElementById("root")
 );
