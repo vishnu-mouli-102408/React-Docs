43a0325b4f16188b91bc012f7c66d35d20907b26
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-30 05:41:37 +0000

    "Initial commit"

--- src/index.js
+++ src/index.js
@@ -3,84 +3,10 @@ import ReactDom from "react-dom";
 import App from "./Components/App"
 
 
-// var today = new Date();
-// var time =
-//   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-// var currentTime = new Date().getHours();
-
-// let customStyle = {
-//   color: "",
-// };
-
-// let greeting;
-
-// if (currentTime < 12) {
-//   greeting = "Good Morning";
-//   customStyle.color = "red";
-// } else if (currentTime < 18) {
-//   greeting = "Good Afternoon";
-//   customStyle.color = "green";
-// } else {
-//   greeting = "Good Night";
-//   customStyle.color = "blue";
-// }
-
-
-// ReactDom.render(
-//   <div>
-//     <h1>Hello {fName + " " + lName}!</h1>
-//     <h1>
-//       Hello {fName} {lName}!
-//     </h1>
-//     <h1> Hello {`${fName} ${lName}`}!</h1>
-//   </div>,
-//   document.getElementById("root")
-// );
-
-const d = new Date();
-let year = d.getFullYear();
-
-// const d = new Date();
-// let year = d.getFullYear();
-
-
-// ReactDom.render(
-//   <div>
-//     <h1>Hello {fName + " " + lName}!</h1>
-//     <h1>
-//       Hello {fName} {lName}!
-//     </h1>
-//     <h1> Hello {`${fName} ${lName}`}!</h1>
-//   </div>,
-//   document.getElementById("root")
-// );
-
 ReactDom.render(
 
   <App/>,
 
-  <div>
-
-
-
-
-    <h1> Hello </h1>
-
-    <p> Created by {`${fName} ${lName}!`} </p>
-    <p> Copyright {year}</p>
-
-
-    <h1> Hello Mouli</h1>
-
-    <h1 className="heading">{greeting}</h1>
-
-
-    {/* <p> Created by {`${fName} ${lName}!`} </p>
-    <p> Copyright {year}</p> */}
-
-
-  </div>,
 
   document.getElementById("root")
 );
