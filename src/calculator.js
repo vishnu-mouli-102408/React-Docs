5fe543015bf16d379dc6d85aa09e8469f7d9cb99
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-30 06:07:53 +0000

    imports, exports and modules in react

---
+++ src/calculator.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function add (n1, n2){
+    return n1+n2
+}
+
+function subtract (n1, n2){
+    return n1-n2
+}
+
+function divide(n1, n2){
+    return n1/n2
+}
+
+function multiply (n1, n2){
+    return n1*n2
+}
+
+export default add;
+export {subtract, divide, multiply};
