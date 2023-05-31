a69c0722d151393e3e7c4bf7ef2a459d0d356fd1
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-31 17:48:15 +0530

    react conditional rendering practise

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -3,22 +3,30 @@ import "../styles.css";
 import Emoji from "./Emoji";
 import emojipedia from "../empjipedia";
 
+import Form from "./Form";
+
+var userRegistered = true;
+
 function App() {
   return (
-    <div>
-      <h1>
-        <span>emojipedia</span>
-      </h1>
-      <dl className="dictionary">
-        {emojipedia.map((item) => (
-          <Emoji
-            key={item.id}
-            emoji={item.emoji}
-            text={item.name}
-            content={item.meaning}
-          />
-        ))}
-      </dl>
+    // <div>
+    //   <h1>
+    //     <span>emojipedia</span>
+    //   </h1>
+    //   <dl className="dictionary">
+    //     {emojipedia.map((item) => (
+    //       <Emoji
+    //         key={item.id}
+    //         emoji={item.emoji}
+    //         text={item.name}
+    //         content={item.meaning}
+    //       />
+    //     ))}
+    //   </dl>
+    // </div>
+
+    <div className="container">
+      <Form isRegistered={userRegistered} />
     </div>
   );
 }
