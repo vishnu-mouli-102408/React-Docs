1b7da11f08a55d1c60fadafd2232b35e293f577d
Ganivada Mouli <vishnumouli0@gmail.com>
2023-06-01 13:00:03 +0530

    object and array destruturing

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../styles.css";
+import cars from "../data.js";
 // import Emoji from "./Emoji";
 // import emojipedia from "../empjipedia";
 
@@ -38,30 +39,62 @@ function App() {
   //   setCount(count - 1);
   // }
 
-  const [time, setTime] = useState(new Date().toLocaleTimeString());
+  // const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  function currentTime() {
-    return setTime(new Date().toLocaleTimeString());
-  }
+  // function currentTime() {
+  //   return setTime(new Date().toLocaleTimeString());
+  // }
 
-  setInterval(currentTime, 1000);
+  // setInterval(currentTime, 1000);
 
+  const [honda, tesla] = cars;
+
+  const {
+    coloursByPopularity: [hondaTopColour],
+  } = honda;
+  const {
+    coloursByPopularity: [teslaTopColour],
+  } = tesla;
+
+  const {
+    speedStats: { topSpeed: hondaTopSpeed },
+  } = honda;
+  const {
+    speedStats: { topSpeed: teslaTopSpeed },
+  } = tesla;
+
+  // <div style={{ textAlign: "center", margin: "auto" }}>
+  //   <h1 style={{ fontSize: "40px" }}>{count}</h1>
+  //   <button style={{ fontSize: "40px" }} onClick={decrease}>
+  //     -
+  //   </button>
+  //   <button style={{ fontSize: "40px" }} onClick={increase}>
+  //     +
+  //   </button>
+  // </div>
+  // <div style={{ textAlign: "center", margin: "auto" }}>
+  //   <h1>{time}</h1>
+  //   <button style={{ fontSize: "40px" }} onClick={currentTime}>
+  //     Get Time
+  //   </button>
+  // </div>
   return (
-    // <div style={{ textAlign: "center", margin: "auto" }}>
-    //   <h1 style={{ fontSize: "40px" }}>{count}</h1>
-    //   <button style={{ fontSize: "40px" }} onClick={decrease}>
-    //     -
-    //   </button>
-    //   <button style={{ fontSize: "40px" }} onClick={increase}>
-    //     +
-    //   </button>
-    // </div>
-    <div style={{ textAlign: "center", margin: "auto" }}>
-      <h1>{time}</h1>
-      <button style={{ fontSize: "40px" }} onClick={currentTime}>
-        Get Time
-      </button>
-    </div>
+    <table>
+      <tr>
+        <th>Brand</th>
+        <th>Top Speed</th>
+      </tr>
+      <tr>
+        <td>{tesla.model}</td>
+        <td>{teslaTopSpeed}</td>
+        <td>{teslaTopColour}</td>
+      </tr>
+      <tr>
+        <td>{honda.model}</td>
+        <td>{hondaTopSpeed}</td>
+        <td>{hondaTopColour}</td>
+      </tr>
+    </table>
   );
 }
 
